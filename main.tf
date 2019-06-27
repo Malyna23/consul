7950a2245b48b27f2bf95233efd74ba3880e738e
Mykola Malyna <mmalyna@softserveinc.com>
2019-06-27 11:07:25 +0300

    Fix credentials path, delete version module

--- main.tf
+++ main.tf
@@ -1,17 +1,14 @@
  module "consul" {
    source = "./modules/consul"
    count_consul = "${var.count_consul}"
-   version = "0.0.1"
    vpc_network = "${module.vpc.consul_network}"
    security_group = "${module.vpc.security_group}"
  }
   module "vpc" {
    source = "./modules/vpc"
-   version = "0.0.1"
  }
  module "consul_certificates" {
    source = "./modules/certificates"
-   version = "0.0.1"
    certs_count = "${var.count_consul}"
    private_key_pem = "${module.ca.ca_private_key_pem}"
    ca_cert_pem  = "${module.ca.ca_cert_pem}"
@@ -21,11 +18,9 @@
  }
  module "ca" {
    source = "./modules/ca"
-   version = "0.0.1"
  }
  module "databases" {
    source = "./modules/databases"
-   version = "0.0.1"
    subnet = "${module.vpc.subnet_databases}"
    security_group = "${module.vpc.security_group}"
  }
